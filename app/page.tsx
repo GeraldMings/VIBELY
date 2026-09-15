358c7cc888cce05b290abfd0de535fa83abc5639
Gerald Mingle <geemingle15@gmail.com>
2026-09-15 14:56:45 +0000

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Vibely</h1>
+      <h2>Meet. Chat. Connect.</h2>
+
+      <p>
+        Make friends, discover people, join communities,
+        and connect with people around the world.
+      </p>
+
+      <button>Create account</button>
+      <button>Sign in</button>
+    </main>
+  );
+}
